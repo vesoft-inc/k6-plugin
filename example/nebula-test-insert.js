2686f652e7e8f4fc67693737071b0136f851b77c
HarrisChu <1726587+HarrisChu@users.noreply.github.com>
2021-08-03 16:03:05 +0800

    update readme

--- example/nebula-test-insert.js
+++ example/nebula-test-insert.js
@@ -6,14 +6,14 @@ import { sleep } from 'k6';
 var lantencyTrend = new Trend('latency');
 var responseTrend = new Trend('responseTime');
 // initial nebula connect pool
-var pool = nebulaPool.initWithSize("192.168.8.61:9669", 400, 4000);
+var pool = nebulaPool.initWithSize("192.168.8.61:9669,192.168.8.62:9669,192.168.8.63:9669", 400, 4000);
 
 // set csv strategy, 1 means each vu has a separate csv reader.
 pool.configCsvStrategy(1)
 
 // initial session for every vu
 var session = pool.getSession("root", "nebula")
-session.execute("USE sf1")
+session.execute("USE ldbc")
 // export let options = {
 // 	stages: [
 // 		{ duration: '2s', target: 20 },
@@ -34,8 +34,9 @@ export default function (data) {
 	// get csv data from csv file
 	let ngql = 'INSERT VERTEX Person(firstName, lastName, gender, birthday, creationDate, locationIP, browserUsed) VALUES '
 	let batches = []
+	let batchSize = 100
 	// batch size 100
-	for (let i = 0; i < 100; i++) {
+	for (let i = 0; i < batchSize; i++) {
 		let d = session.getData();
 		let values = []
 		// concat the insert value
